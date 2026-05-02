6004243fda6d98cc7af6e66c0619c466d742aace
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-05-02 16:25:03 +0530

    Fixed the Print Statements

--- src/EasyLeetcode/342_PowerOfFour.java
+++ src/EasyLeetcode/342_PowerOfFour.java
@@ -6,14 +6,14 @@ class PowerOfFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the Number: ");
+        System.out.print("Enter the Number: ");
         int n = sc.nextInt();
 
         if(n >=1 && (n & (n-1)) == 0 && (n-1) % 3 == 0) {
             System.out.println("Number is A Power of 4");
+        } else {
+            System.out.println("Number is Not a Power of 4");
         }
-
-        System.out.println("Number is Not a Power of 4");
         sc.close();
     }
 }
