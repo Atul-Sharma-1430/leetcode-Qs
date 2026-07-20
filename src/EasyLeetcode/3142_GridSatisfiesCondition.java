e5b659f46638aabcdc0db2d4bd166a62638ed4a0
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-20 17:57:20 +0530

    Solved LC 3142

---
+++ src/EasyLeetcode/3142_GridSatisfiesCondition.java
@@ -0,0 +1,50 @@
+package EasyLeetcode;
+
+import java.util.Scanner;
+
+class GridSatisfiesCondition {
+    public static boolean satisfiesConditions(int[][] grid) {
+
+        // Check horizontally
+        for (int i = 0; i < grid[0].length - 1; i++) {
+            if (grid[0][i] == grid[0][i + 1]) {
+                return false;
+            }
+        }
+
+        // Check vertically
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] != grid[i + 1][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] grid = new int[rows][cols];
+
+        System.out.print("Enter the elements of the grid:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean ans = satisfiesConditions(grid);
+        System.out.println("Result: " + ans);
+
+        sc.close();
+    }
+}
