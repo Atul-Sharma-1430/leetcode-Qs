fa46c41a6d0ab8395539813ee47590ae32d98f52
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-25 22:56:42 +0530

    Solved LC 48 Rotate Image by 90 deg using brute force approach

---
+++ src/MediumLeetcode/48_RotateImageBy90Deg.java
@@ -0,0 +1,55 @@
+package MediumLeetcode;
+
+import java.util.Scanner;
+
+class RotateImageBy90Deg {
+    // TC : O(n^2)
+    // SC : O(n^2)
+    public static void rotateImage(int[][] matrix) {
+        int [][] ansMatrix = new int[matrix.length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                ansMatrix[j][matrix.length - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = ansMatrix[i][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int columns = sc.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        rotateImage(matrix);
+
+        System.out.println("Matrix after rotation:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
