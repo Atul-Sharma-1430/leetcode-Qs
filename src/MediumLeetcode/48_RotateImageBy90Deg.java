76f035dd5dbd45216089583f21c96f68e1bc9db9
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-29 00:36:12 +0530

    Solved LC 48 Rotate image Q with optimal approach

--- src/MediumLeetcode/48_RotateImageBy90Deg.java
+++ src/MediumLeetcode/48_RotateImageBy90Deg.java
@@ -21,6 +21,31 @@ class RotateImageBy90Deg {
         }
     }
 
+    // TC : O(n2)
+    // SC : O(1)
+    public static void rotateImage2(int[][] matrix) {
+
+        // Transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i < j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
