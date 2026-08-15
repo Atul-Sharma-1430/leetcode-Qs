7e539beaa2f49549f875b7c29726d5648bf861b3
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-08-16 00:36:56 +0530

    Time: 15 ms (5.14%), Space: 47.3 MB (5.46%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        String rev = "";
+
+        int i = s.length() - 1;
+        while (i >= 0) {
+
+            // Agar end me spaces hai toh vo sab skip krke first word pe pahucho right side se
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+
+            // Agar string end ho gya toh leave kr do
+            if (i < 0) {
+                break;
+            }
+
+            // Ab ye j jo word mila us word ke st letter tak pahuchne keliye
+            int j = i;
+            while (j >= 0 && s.charAt(j) != ' ') {
+                j--;
+            }
+
+            // Vo wale word ko left se right copy kro
+            for (int k = j + 1; k <= i; k++) {
+                rev += s.charAt(k);
+            }
+            
+            // and space add kro
+            rev += " ";
+
+            // Fir add krne ke baad prev word ke last char pe pahucho
+            i = j;
+        }
+
+        
+        // first word add krne ke baad bhi ek aur baar space add ho jaayega toh isliye trim kr do 
+        return rev.trim();
+    }
+}
