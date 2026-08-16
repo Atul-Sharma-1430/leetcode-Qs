44ca673f12decebb837a2d0c264cc4d9f26cf668
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-08-16 22:39:11 +0530

    Time: 4 ms (92.82%), Space: 43.8 MB (95.21%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
-
-        String rev = "";
+        // Solution 2 - Using StringBuilder
+        StringBuilder rev = new StringBuilder("");
 
         int i = s.length() - 1;
         while (i >= 0) {
@@ -24,18 +24,61 @@ class Solution {
 
             // Vo wale word ko left se right copy kro
             for (int k = j + 1; k <= i; k++) {
-                rev += s.charAt(k);
+                rev.append(s.charAt(k));
             }
-            
+
             // and space add kro
-            rev += " ";
+            rev.append(" ");
 
             // Fir add krne ke baad prev word ke last char pe pahucho
             i = j;
         }
 
-        
         // first word add krne ke baad bhi ek aur baar space add ho jaayega toh isliye trim kr do 
-        return rev.trim();
+        return rev.toString().trim();
+
+
+
+
+
+
+        // Solution 1
+        //This logic is optimal but using String can cause inefficiency cz for each word we are creating a new string as Strngs are immutable hence we should use StringBuilder
+
+        // String rev = "";
+
+        // int i = s.length() - 1;
+        // while (i >= 0) {
+
+        //     // Agar end me spaces hai toh vo sab skip krke first word pe pahucho right side se
+        //     while (i >= 0 && s.charAt(i) == ' ') {
+        //         i--;
+        //     }
+
+        //     // Agar string end ho gya toh leave kr do
+        //     if (i < 0) {
+        //         break;
+        //     }
+
+        //     // Ab ye j jo word mila us word ke st letter tak pahuchne keliye
+        //     int j = i;
+        //     while (j >= 0 && s.charAt(j) != ' ') {
+        //         j--;
+        //     }
+
+        //     // Vo wale word ko left se right copy kro
+        //     for (int k = j + 1; k <= i; k++) {
+        //         rev += s.charAt(k);
+        //     }
+
+        //     // and space add kro
+        //     rev += " ";
+
+        //     // Fir add krne ke baad prev word ke last char pe pahucho
+        //     i = j;
+        // }
+
+        // // first word add krne ke baad bhi ek aur baar space add ho jaayega toh isliye trim kr do 
+        // return rev.trim();
     }
 }
