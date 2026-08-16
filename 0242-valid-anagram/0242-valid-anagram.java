4b91af2424652c9c407ae83efe8a1b11898ecd4e
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-08-16 23:24:12 +0530

    Time: 6 ms (43.27%), Space: 44.1 MB (89.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -21,18 +21,44 @@ class Solution {
 
 
         // Soln 2
-        if (s.length() != t.length()) return false;
+        // if (s.length() != t.length()) return false;
 
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        // char[] arr1 = s.toCharArray();
+        // char[] arr2 = t.toCharArray();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
 
-        s = new String(arr1);
-        t = new String(arr2);
+        // s = new String(arr1);
+        // t = new String(arr2);
 
-        return s.equals(t);
+        // return s.equals(t);
 
+
+
+
+        // Soln 3
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            // s ke character ki frequency increase
+            freq[s.charAt(i) - 'a']++;
+
+            // same time pe t ke character ki frequency bhi decrease
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        // Agar sabki frequency 0 hai toh anagram hai
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
