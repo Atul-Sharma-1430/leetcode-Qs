369776e958122923d38b81696f0f55f2d903ddbc
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-08-27 13:31:44 +0530

    Time: 3 ms (69.34%), Space: 48.9 MB (90.92%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,30 +1,12 @@
-class Solution { 
-    public int[] replaceElements(int[] arr) { 
-
-        int max = arr[arr.length - 1];  
-
-        // Right se left traverse karenge
-        for (int i = arr.length - 1; i >= 0; i--) { 
-
-            // Last element ke right mein koi element nahi hai
-            if (i == arr.length - 1) { 
-                max = arr[i];      // Last element ko max bana do
-                arr[i] = -1;       // Last element ko -1 se replace karo
-                continue; 
-            } 
-
-            // Current element ko temporarily store karo
-            int curr = arr[i]; 
-
-            // Current element ko right side ke greatest element se replace karo
-            arr[i] = max; 
-
-            // Agar current element max se bada hai, toh current element ko new maximum bana do
-            if (curr > max) { 
-                max = curr; 
-            } 
-        } 
-
-        return arr; 
-    } 
+class Solution {
+    public int[] replaceElements(int[] arr) {
+       int maxi = arr[arr.length-1]; 
+       arr[arr.length-1]=-1;
+for(int i=arr.length-2;i>=0;i--){
+    int temp =arr[i];
+    arr[i] = maxi;
+    maxi = Math.max(maxi,temp);
+}
+return arr;
+    }
 }
