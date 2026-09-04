0db53ecdcc0f969ab30b59ecc32a490b64233041
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-09-04 21:57:26 +0530

    Time: 1 ms (99.79%), Space: 46 MB (97.45%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,25 +1,55 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-
+        
+        // Soltuion 1
+        // TC : O(n)
+        // SC : O(n)
+        int[] array = new int[nums.length];
         int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-            }
+            max = Math.max(nums[i], max);
+            array[i] = max;
+        }
 
-            int min = Integer.MAX_VALUE;
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] < min) {
-                    min = nums[j];
-                }
-            }
+        int min = Integer.MAX_VALUE;
+        int ans = -1;
 
-            if ((max - min) <= k) {
-                return i;
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            min = Math.min(nums[i], min);
+            array[i] = array[i] - min;
+
+            if (array[i] <= k) {
+                ans = i;
             }
         }
 
-        return -1;
+        return ans;
+
+
+        // Solution 2
+        // TC : O(n^2)
+        // SC : O(1)
+        // int max = Integer.MIN_VALUE;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] > max) {
+        //         max = nums[i];
+        //     }
+
+        //     int min = Integer.MAX_VALUE;
+        //     for (int j = i; j < nums.length; j++) {
+        //         if (nums[j] < min) {
+        //             min = nums[j];
+        //         }
+        //     }
+
+        //     if ((max - min) <= k) {
+        //         return i;
+        //     }
+        // }
+
+        // return -1;
     }
 }
