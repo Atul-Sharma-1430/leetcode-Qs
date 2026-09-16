54f8a82c0d80830e5aed7d82d736ed81bbac09f7
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-09-16 20:40:07 +0530

    Time: 1 ms (98.76%), Space: 42.9 MB (20.86%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,47 +1,81 @@
 class Solution {
     public long countCommas(long n) {
+        // Solution 2
+        long commas = 0;
 
-        // 1000 se chhote numbers mein koi comma nahi hota
-        if (n < 1000) {
-            return 0;
+        // agar n 1000 se bada hua toh 1000 se lekar n tak jitne commas aayenge vo sab add honge
+        if (n >= 1000) {
+            commas += n - 999;
         }
 
-        // final ans total commas
-        long commas = 0;
-        // current range me kitne number rhenge vo rakhne ke loiye
-        long totalNums = 0;
+        // agar n 1000000 se bada hua toh 1000000 se lekar n tak jitne extra commas aayenge vo sab add honge
+        if (n >= 1000000) {
+            commas += n - 999999;
+        }
 
-        // har range ka start valye store krne ke liye
-        // 1st comma 1000 se shuru hota h
-        long start = 1000;
+        // agar n 1000000000 se bada hua toh 1000000000 se lekar n tak jitne extra commas aayenge vo sab add honge
+        if (n >= 1000000000L) {
+            commas += n - 999999999;
+        }
 
-        // har range me kitne commas rhenge usko store krunga
-        long commaCount = 1;
+        // agar n 1000000000000 se bada hua toh 1000000000000 se lekar n tak jitne extra commas aayenge vo sab add honge
+        if (n >= 1000000000000L) {
+            commas += n - 999999999999L;
+        }
 
-        // har range ke liye loop chalga and check krenge ki kya range ka st n se kam hai ki nhi
-        while (start <= n) {
-
-            // Current range ka last number nikalenge
-            long end = start * 1000 - 1;
-
-            // aga us range ka last number(end) n se bda hua toh last numbe(end) me n daal denge
-            if (end > n) {
-                end = n;
-            }
-
-            // fir count krenge ki vo range me kitne total number hai
-            totalNums = end - start + 1;
-
-            // fir us range me har number me jitne commas rhenge usko numbner se multiply kr denge to get total commas in trhat range and add kr denge
-            commas += totalNums * commaCount;
-
-            // next range ke start pe jaynge jisme comma count 2 rhegas
-            start *= 1000;
-
-            // isliye comma ko 1 se increase
-            commaCount++;
+        // agar n 1000000000000000 se bada hua toh 1000000000000000 se lekar n tak jitne extra commas aayenge vo sab add honge
+        if (n >= 1000000000000000L) {
+            commas += n - 999999999999999L;
         }
 
         return commas;
+
+
+
+
+
+        // Solution 1
+        // // 1000 se chhote numbers mein koi comma nahi hota
+        // if (n < 1000) {
+        //     return 0;
+        // }
+
+        // // final ans total commas
+        // long commas = 0;
+        // // current range me kitne number rhenge vo rakhne ke loiye
+        // long totalNums = 0;
+
+        // // har range ka start valye store krne ke liye
+        // // 1st comma 1000 se shuru hota h
+        // long start = 1000;
+
+        // // har range me kitne commas rhenge usko store krunga
+        // long commaCount = 1;
+
+        // // har range ke liye loop chalga and check krenge ki kya range ka st n se kam hai ki nhi
+        // while (start <= n) {
+
+        //     // Current range ka last number nikalenge
+        //     long end = start * 1000 - 1;
+
+        //     // aga us range ka last number(end) n se bda hua toh last numbe(end) me n daal denge
+        //     if (end > n) {
+        //         end = n;
+        //     }
+
+        //     // fir count krenge ki vo range me kitne total number hai
+        //     totalNums = end - start + 1;
+
+        //     // fir us range me har number me jitne commas rhenge usko numbner se multiply kr denge to get total commas in trhat range and add kr denge
+        //     commas += totalNums * commaCount;
+
+        //     // next range ke start pe jaynge jisme comma count 2 rhegas
+        //     start *= 1000;
+
+        //     // isliye comma ko 1 se increase
+        //     commaCount++;
+        // }
+
+        // return commas;
     }
 }
