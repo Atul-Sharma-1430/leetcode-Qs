3c88aa781ddfc0318c78ad847480cbe2e6cfc314
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-09-18 21:44:11 +0530

    Time: 0 ms (100%), Space: 45.4 MB (51.38%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,9 +11,9 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
-        // Soltuion 1 
+        // Soltuion 3
         // TC : O(n)
-        // SC : O(ansLL.size)
+        // SC : O(1)
 
         // for khali list
         if (head == null) {
@@ -25,35 +25,87 @@ class Solution {
             return head;
         }
 
+        // ye bas final ans nodes mke liye
         ListNode temp = head;
 
         // since array is sorted so we can use prev variable to track prev value;
         int prev = temp.val;
 
-        // ans list
-        ListNode ans = new ListNode(temp.val);
-        // to trck ans LL
-        ListNode temp2 = ans;
+        // ye basically traverse krne ke liye
+        ListNode temp2 = head;
 
-        while (temp != null) {
+        while (temp2 != null) {
 
             // agar value already present hai toh duplicate hai, isliye skip karo
-            if (temp.val == prev) {
-                prev = temp.val;
-                temp = temp.next;
+            if (temp2.val == prev) {
+                temp2 = temp2.next;
                 continue;
             }
 
-            prev = temp.val;
+            // else new value ko as prev store kro
+            prev = temp2.val;
 
-            // make new node and add too ans
-            ListNode newNode = new ListNode(temp.val);
-            temp2.next = newNode;
-            temp = temp.next;
+            // temp ke next ko temp2 pe point kr do
+            temp.next = temp2;
+            // temp ko temp 2 me move kr do
+            temp = temp2;
+            // and temp2 ko next kr do
             temp2 = temp2.next;
         }
+        
+        // last unique node ke baad kuch nahi hoga
+        temp.next = null;
 
-        return ans;
+        return head;
+
+
+
+
+
+
+        // // Soltuion 2
+        // // TC : O(n)
+        // // SC : O(ansLL.size)
+
+        // // for khali list
+        // if (head == null) {
+        //     return null;
+        // }
+
+        // // for hd only
+        // if (head.next == null) {
+        //     return head;
+        // }
+
+        // ListNode temp = head;
+
+        // // since array is sorted so we can use prev variable to track prev value;
+        // int prev = temp.val;
+
+        // // ans list
+        // ListNode ans = new ListNode(temp.val);
+        // // to trck ans LL
+        // ListNode temp2 = ans;
+
+        // while (temp != null) {
+
+        //     // agar value already present hai toh duplicate hai, isliye skip karo
+        //     if (temp.val == prev) {
+        //         prev = temp.val;
+        //         temp = temp.next;
+        //         continue;
+        //     }
+
+        //     prev = temp.val;
+
+        //     // make new node and add too ans
+        //     ListNode newNode = new ListNode(temp.val);
+        //     temp2.next = newNode;
+        //     temp = temp.next;
+        //     temp2 = temp2.next;
+        // }
+
+        // return ans;
 
 
 
