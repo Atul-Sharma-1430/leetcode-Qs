a88e1bc143ebc78fcb9d3d0517122b2b31f6cd8c
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-09-18 20:51:37 +0530

    Time: 1 ms (4.22%), Space: 46.4 MB (5.65%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,6 +11,10 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
+        // Soltuion 1 
+        // TC : O(n)
+        // SC : O(ansLL.size)
+
         // for khali list
         if (head == null) {
             return null;
@@ -21,29 +25,26 @@ class Solution {
             return head;
         }
 
-        // values -100 se 100 tak ho sakti hain isliye index ke liye +100 kiya
-        boolean[] wasPresent = new boolean[201];
-
         ListNode temp = head;
 
-        // mark that head is alrady prsnt cz vo anyhow rhega hi
-        wasPresent[temp.val + 100] = true;
+        // since array is sorted so we can use prev variable to track prev value;
+        int prev = temp.val;
 
         // ans list
         ListNode ans = new ListNode(temp.val);
-        //k to trck ans LL
+        // to trck ans LL
         ListNode temp2 = ans;
 
         while (temp != null) {
 
             // agar value already present hai toh duplicate hai, isliye skip karo
-            if (wasPresent[temp.val + 100]) {
+            if (temp.val == prev) {
+                prev = temp.val;
                 temp = temp.next;
                 continue;
             }
 
-            // value ko present mark karo
-            wasPresent[temp.val + 100] = true;
+            prev = temp.val;
 
             // make new node and add too ans
             ListNode newNode = new ListNode(temp.val);
@@ -53,5 +54,57 @@ class Solution {
         }
 
         return ans;
+
+
+
+
+
+
+        // Soltuion 1 
+        // TC : O(n)
+        // SC : O(201) + O(ansLL.size)
+
+        // // for khali list
+        // if (head == null) {
+        //     return null;
+        // }
+
+        // // for hd only
+        // if (head.next == null) {
+        //     return head;
+        // }
+
+        // // values -100 se 100 tak ho sakti hain isliye index ke liye +100 kiya
+        // boolean[] wasPresent = new boolean[201];
+
+        // ListNode temp = head;
+
+        // // mark that head is alrady prsnt cz vo anyhow rhega hi
+        // wasPresent[temp.val + 100] = true;
+
+        // // ans list
+        // ListNode ans = new ListNode(temp.val);
+        // // to trck ans LL
+        // ListNode temp2 = ans;
+
+        // while (temp != null) {
+
+        //     // agar value already present hai toh duplicate hai, isliye skip karo
+        //     if (wasPresent[temp.val + 100]) {
+        //         temp = temp.next;
+        //         continue;
+        //     }
+
+        //     // value ko present mark karo
+        //     wasPresent[temp.val + 100] = true;
+
+        //     // make new node and add too ans
+        //     ListNode newNode = new ListNode(temp.val);
+        //     temp2.next = newNode;
+        //     temp = temp.next;
+        //     temp2 = temp2.next;
+        // }
+
+        // return ans;
     }
 }
