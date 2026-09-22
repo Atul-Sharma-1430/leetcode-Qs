1298fe4019e40cc2a6bbd8f2038155ba9aec248e
Atul Sharma <216697914+Atul-Sharma-1430@users.noreply.github.com>
2026-09-22 23:54:13 +0530

    Time: 0 ms (100%), Space: 44.9 MB (55.34%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,55 +10,107 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        // Soln 1 
-        // TC : O(n)
-        // SC : O(n) --> using freq arrayu
 
-        // for khali list
+        // Soln 2
+        // TC : O(n)
+        // SC : O(1)
         if (head == null) {
             return null;
         }
 
-        // for head only
-        if (head.next == null) {
-            return head;
-        }
+        // dummy head rhega cz we need a st pt
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
-        // sinze range is -100 to 100
-        int[] freq = new int[201];
+        ListNode temp = dummy; // to traverse
 
-        // ye original LL ko traverse krne ke liye
-        ListNode temp = head;
+        while (head != null && head.next != null) {
 
-        // har value ki frequency count karo
-        while (temp != null) {
-            freq[temp.val + 100]++;
-            temp = temp.next;
-        }
+            // agar duplicate mila
+            if (head.val == head.next.val) {
 
-        // dummy node banaya final answer LL ke liye
-        ListNode newHead = new ListNode(0);
+                int duplicate = head.val; // duplicate value stored
+                // jab tak same value hai skip krt rho
+                while (head != null && head.val == duplicate) {
+                    head = head.next;
+                }
 
-        // ye answer LL ko vo karne ke liye
-        temp = newHead;
+                // temp ko duplicate ke baad wale node pe point kr do
+                temp.next = head;
 
-        // frequency array ko traverse karo
-        for (int i = 0; i < freq.length; i++) {
-            
-            // agar value sirf ek baar present hai toh usko answer LL mein add karo
-            if (freq[i] == 1) {
-                // index se original value nikalo
-                ListNode newNode = new ListNode(i - 100);
-
-                // new node ko answer LL mein add karo
-                temp.next = newNode;
-
-                // temp ko newly added node pe move karo
+            } else {
+                // agar duplicate nahi hai toh dono ko move kro
                 temp = temp.next;
+                head = head.next;
             }
         }
 
-        // dummy node ko skip karke actual answer return karo
-        return newHead.next;
+        return dummy.next;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // // Soln 1 
+        // // TC : O(n)
+        // // SC : O(n) --> using freq arrayu
+
+        // // for khali list
+        // if (head == null) {
+        //     return null;
+        // }
+
+        // // for head only
+        // if (head.next == null) {
+        //     return head;
+        // }
+
+        // // sinze range is -100 to 100
+        // int[] freq = new int[201];
+
+        // // ye original LL ko traverse krne ke liye
+        // ListNode temp = head;
+
+        // // har value ki frequency count karo
+        // while (temp != null) {
+        //     freq[temp.val + 100]++;
+        //     temp = temp.next;
+        // }
+
+        // // dummy node banaya final answer LL ke liye
+        // ListNode newHead = new ListNode(0);
+
+        // // ye answer LL ko build karne ke liye
+        // temp = newHead;
+
+        // // frequency array ko traverse karo
+        // for (int i = 0; i < freq.length; i++) {
+
+        //     // agar value sirf ek baar present hai toh usko answer LL mein add karo
+        //     if (freq[i] == 1) {
+
+        //         // index se original value nikalo
+        //         ListNode newNode = new ListNode(i - 100);
+
+        //         // new node ko answer LL mein add karo
+        //         temp.next = newNode;
+
+        //         // temp ko newly added node pe move karo
+        //         temp = temp.next;
+        //     }
+        // }
+
+        // // dummy node ko skip karke actual answer return karo
+        // return newHead.next;
     }
 }
